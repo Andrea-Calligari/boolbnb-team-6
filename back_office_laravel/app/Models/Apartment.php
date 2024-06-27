c0ec16b45c392cb7133e05a55e3567d2e24bd37a
MarinaLasorsa <marina.lasorsa@gmail.com>
2024-06-27 17:28:04 +0200

    ADD: Apartment model with controller, migration, seeder, requests and factory

---
+++ back_office_laravel/app/Models/Apartment.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+//use App\Models\User;
+//use App\Models\Category;
+use Illuminate\Database\Eloquent\Model;
+
+class Apartment extends Model
+{
+    use HasFactory;
+
+    protected $fillable = ['title', 'slug', 'description', 'price', 'rooms_number', 'beds_number', 'baths_number', 'mtq', 'address', 'latitude', 'longitude', 'image', 'visible'];
+
+    //public function user(){
+        //return $this->belongsTo(User::class);
+    //}
+
+    //public function category(){
+        //return $this->belongsTo(Category::class);
+    //}
+}
