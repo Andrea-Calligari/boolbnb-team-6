b5189b01a7bc099b4782be7c18b4d6ee079b7484
AntoCic <anto.cic.127@gmail.com>
2024-06-27 20:21:39 +0200

    ADD start api works

--- back_office_laravel/database/seeders/DatabaseSeeder.php
+++ back_office_laravel/database/seeders/DatabaseSeeder.php
@@ -1,8 +1,11 @@
 <?php
 
 namespace Database\Seeders;
- use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+
+use function Laravel\Prompts\password;
 
 class DatabaseSeeder extends Seeder
 {
