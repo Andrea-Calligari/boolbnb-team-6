f8f9a93c617849a0a0026cf0880312953dde346a
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-06-28 13:03:44 +0200

    Addes:  services-in-ServiceSeeder

--- back_office_laravel/database/seeders/ViewSeeder.php
+++ back_office_laravel/database/seeders/ViewSeeder.php
@@ -5,14 +5,19 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\View;
+
 class ViewSeeder extends Seeder
 {
     /**
      * Run the database seeds.
      */
     public function run(): void
-    {   $view = new View();
-        $view->ip_address = '192.168.0.1';
-        $view->save();
+    {
+        for ($i = 0; $i < 17; $i++) {
+
+            $view = new View();
+            $view->ip_address = '192.168.0.1';
+            $view->save();
+        }
     }
 }
