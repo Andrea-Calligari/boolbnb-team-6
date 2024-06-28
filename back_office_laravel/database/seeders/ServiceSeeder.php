242b8bce5375da045c0a215eaceabff7b5f98c0b
Andrea-Calligari <andrea.calligari26@gmail.com>
2024-06-28 13:03:44 +0200

    Addes:  services-in-ServiceSeeder

--- back_office_laravel/database/seeders/ServiceSeeder.php
+++ back_office_laravel/database/seeders/ServiceSeeder.php
@@ -15,9 +15,22 @@ class ServiceSeeder extends Seeder
      */
     public function run(Faker $faker): void
     {
-        $service = new Service();
-        $service->name = $faker->name();
-        $service->icon = 'https://th.bing.com/th/id/OIP.o0olEd3NKcmoQL9-oyt8bAHaHa?rs=1&pid=ImgDetMain';
-        $service->save();
+        $services = [ 
+            'Wifi', 'Parcheggio', 'Aria Condizionata', 'carta Igienica', 'Shampoo', 'Self-check-in', 
+            'Cucina', 'Piscina', 'Vasca Idromassaggio', 'Lavatrice', 'Tv', 'Riscaldamento',
+            'Allarme antincendio', 'Camino', 'Kit di primo soccorso', 'Ascensore', 
+            'Colazione', 'Lavastoviglie', 'Macchina del Caffe', 'Frigorifero', 'Freezer',
+            'Tostapane', 'Cassaforte','Porta Blindata', 'Microonde', 'Giochi da tavolo', 'Fasciatoio', 
+            'Asciugacapelli','Asciugatrice',"Balcone", 'Giardino', 'Palestra', 'Barbeque', 'Rilevatore di monossido di carbonio', 'Scrivania',
+            'Stampante', ' Animali consentiti', 'Area Fumatori', 'Cuccia', 'Servizi di pulizia ','Servizi di manutenzione', 'Ingresso privato'
+        ];
+
+        foreach($services as $service){
+            $service = new Service();
+            $service->name = $service;
+            $service->icon = 'https://th.bing.com/th/id/OIP.o0olEd3NKcmoQL9-oyt8bAHaHa?rs=1&pid=ImgDetMain';
+            $service->save();
+
+        }
     }
 }
