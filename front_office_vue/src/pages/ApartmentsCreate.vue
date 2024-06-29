ca5722fee99f5a23fabfd43e7662d85b69733edc
AntoCic <anto.cic.127@gmail.com>
2024-06-29 21:22:19 +0200

    ADD style create

--- front_office_vue/src/pages/ApartmentsCreate.vue
+++ front_office_vue/src/pages/ApartmentsCreate.vue
@@ -14,8 +14,8 @@
 
             <div class="mb-3">
                 <label for="description" class="form-label">Descrizione</label>
-                <textarea class="form-control" v-model="description" rows="3" :class="classValidate(isVdescription)" id="description"
-                    name="description" placeholder="Inserisci Descrizione"></textarea>
+                <textarea class="form-control" v-model="description" rows="3" :class="classValidate(isVdescription)"
+                    id="description" name="description" placeholder="Inserisci Descrizione"></textarea>
                 <div v-if="classValidate(isVdescription) === 'is-invalid'" class="mt-0 text-danger">
                     Il testo non deve superare i 1000 caratteri
                 </div>
@@ -23,8 +23,8 @@
 
             <div class="mb-3">
                 <label for="price" class="form-label">Prezzo</label>
-                <input type="number" class="form-control" v-model="price" :class="classValidate(isVprice)" step="0.01" id="price" name="price"
-                    placeholder="Inserisci Prezzo">
+                <input type="number" class="form-control" v-model="price" :class="classValidate(isVprice)" step="0.01"
+                    id="price" name="price" placeholder="Inserisci Prezzo">
                 <div v-if="classValidate(isVprice) === 'is-invalid'" class="mt-0 text-danger">
                     Il prezzo massimo è di 9999,99 e non può essere vuoto
                 </div>
@@ -33,8 +33,8 @@
 
             <div class="mb-3">
                 <label for="rooms_number" class="form-label">N° Stanze</label>
-                <input type="number" class="form-control" v-model="rooms" :class="classValidate(isVrooms)" id="rooms_number"
-                    name="rooms_number" placeholder="Inserisci N° Stanze">
+                <input type="number" class="form-control" v-model="rooms" :class="classValidate(isVrooms)"
+                    id="rooms_number" name="rooms_number" placeholder="Inserisci N° Stanze">
                 <div v-if="classValidate(isVrooms) === 'is-invalid'" class="mt-0 text-danger">
                     l'inserzione deve avere almeno una stanza
                 </div>
@@ -42,8 +42,8 @@
 
             <div class="mb-3">
                 <label for="beds_number" class="form-label">N° letti</label>
-                <input type="number" class="form-control" v-model="beds" :class="classValidate(isVbeds)" id="beds_number"
-                    name="beds_number" placeholder="Inserisci N° letti">
+                <input type="number" class="form-control" v-model="beds" :class="classValidate(isVbeds)"
+                    id="beds_number" name="beds_number" placeholder="Inserisci N° letti">
                 <div v-if="classValidate(isVbeds) === 'is-invalid'" class="mt-0 text-danger">
                     l'inserzione deve avere almeno un posto letto
                 </div>
@@ -52,8 +52,8 @@
 
             <div class="mb-3">
                 <label for="baths_number" class="form-label">N˚ bagni</label>
-                <input type="number" class="form-control" v-model="baths" :class="classValidate(isVbaths)" id="baths_number"
-                    name="baths_number" placeholder="Inserisci N˚ bagni">
+                <input type="number" class="form-control" v-model="baths" :class="classValidate(isVbaths)"
+                    id="baths_number" name="baths_number" placeholder="Inserisci N˚ bagni">
                 <div v-if="classValidate(isVbaths) === 'is-invalid'" class="mt-0 text-danger">
                     l'inserzione deve avere almeno un bagno
                 </div>
@@ -61,7 +61,7 @@
 
             <div class="mb-3">
                 <label for="mtq" class="form-label">Metri quadri</label>
-                <input type="number" class="form-control" :class="classValidate(isVmtq)"  id="mtq" name="mtq"
+                <input type="number" class="form-control" :class="classValidate(isVmtq)" id="mtq" name="mtq"
                     v-model="mtq" placeholder="Inserisci Metri quadri">
                 <div v-if="classValidate(isVmtq) === 'is-invalid'" class="mt-0 text-danger">
                     l'inserzione deve avere come metratura minima 3mtq
@@ -94,20 +94,21 @@
             <div class="mb-3">
                 <label for="category" class="form-label">categorie</label>
                 <select name="category" v-model="category" id="category">
-                    <option v-for="cateGory in store.options.categories" :key="cateGory.id"  :value="cateGory.id">{{ cateGory.name }}</option>
-                    
+                    <option v-for="cateGory in store.options.categories" :key="cateGory.id" :value="cateGory.id">{{
+                        cateGory.name }}</option>
+
                 </select>
             </div>
 
-            <div class="mb-3">
-                <template v-for="serVice in store.options.services">
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-lg-4 mb-3">
+                <div class="col" v-for="serVice in store.options.services">
                     <input type="checkbox" :id="serVice.name" :value="serVice.id" v-model="services">
                     <label :for="serVice.name">{{ serVice.name }}</label>
-                </template>
+                </div>
             </div>
 
-            
-            
+
+
             <div class="mb-3">
                 <label for="visible" class="form-label">Visibile</label>
                 <select name="visible" v-model="visible" id="visible">
@@ -140,23 +141,23 @@ export default {
             mtq: '',
             position: null,
             description: '',
-            isVdescription:null,
+            isVdescription: null,
             price: 1,
-            isVprice:null,
-            rooms:1,
-            isVrooms:null,
-            beds:1,
-            isVbeds:null,
-            baths:1,
-            isVbaths:null,
-            mtq:1,
-            isVmtq:null,
+            isVprice: null,
+            rooms: 1,
+            isVrooms: null,
+            beds: 1,
+            isVbeds: null,
+            baths: 1,
+            isVbaths: null,
+            mtq: 1,
+            isVmtq: null,
             visible: '1',
-            isVvisible:null,
-            image:'https://picsum.photos/200/300',
-            isVimage:null,
-            category:1,
-            services:[]
+            isVvisible: null,
+            image: 'https://picsum.photos/200/300',
+            isVimage: null,
+            category: 1,
+            services: []
 
         }
     },
@@ -173,14 +174,14 @@ export default {
             }
         },
         isFormValidated() {
-            
+
             this.isVtitle = this.store.validateString(this.title)
-            this.isVdescription = this.store.validateString(this.description,0,1000)
+            this.isVdescription = this.store.validateString(this.description, 0, 1000)
             this.isVprice = this.store.validateDecimal(this.price)
             this.isVrooms = this.store.validateInteger(this.rooms)
             this.isVbeds = this.store.validateInteger(this.beds)
             this.isVbaths = this.store.validateInteger(this.baths)
-            this.isVmtq = this.store.validateInteger(this.mtq,1,99999)
+            this.isVmtq = this.store.validateInteger(this.mtq, 1, 99999)
             this.isVaddress = this.store.validateString(this.address)
             // validate image 
             this.isVvisible = this.store.validateBoolean(this.visible)
@@ -194,7 +195,7 @@ export default {
                 this.isVbaths &&
                 this.isVmtq &&
                 this.isVaddress &&
-                this.isVvisible 
+                this.isVvisible
             ) {
                 return true
             } else {
@@ -215,30 +216,30 @@ export default {
 
                 console.log('FINALMENTE sei arrivato qua');
 
-                 //this.store.user = 0;
-                 //await axios.get("http://localhost:8000/sanctum/csrf-cookie");
-                 await axios.post("http://localhost:8000/api/apartments",{
-                    title:this.title,
-                    description:this.description,
-                    price:this.price,
-                    rooms_number:this.rooms,
-                    beds_number:this.beds,
-                    baths_number:this.baths,
-                    mtq:this.mtq,
-                    address:this.address,
+                //this.store.user = 0;
+                //await axios.get("http://localhost:8000/sanctum/csrf-cookie");
+                await axios.post("http://localhost:8000/api/apartments", {
+                    title: this.title,
+                    description: this.description,
+                    price: this.price,
+                    rooms_number: this.rooms,
+                    beds_number: this.beds,
+                    baths_number: this.baths,
+                    mtq: this.mtq,
+                    address: this.address,
                     latitude: this.position.lat,
                     longitude: this.position.lon,
-                    image:this.image,
-                    visible:this.visible,
-                    user_id:this.store.user.id,
-                    category_id:this.category,
-                    services_ids:this.services,
-                 }).then((res) => {
+                    image: this.image,
+                    visible: this.visible,
+                    user_id: this.store.user.id,
+                    category_id: this.category,
+                    services_ids: this.services,
+                }).then((res) => {
                     const apartmentSlug = res.data.apartment.slug;
-                    this.$router.push({name:'apartment', params:{slug:apartmentSlug}});
-                 }).catch((err) => {
+                    this.$router.push({ name: 'apartment', params: { slug: apartmentSlug } });
+                }).catch((err) => {
                     console.log(err.response.data.message);
-                 });
+                });
             }
         },
     },
