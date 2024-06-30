fc1373d5bb1910520594cf441c6301dc271c5d66
AntoCic <anto.cic.127@gmail.com>
2024-06-30 11:55:53 +0200

    ADD route delete

--- front_office_vue/src/router.js
+++ front_office_vue/src/router.js
@@ -6,12 +6,14 @@ import ApartmentsView from './pages/ApartmentsView.vue'
 import ApartmentView from './pages/ApartmentView.vue'
 import ApartmentsCreate from './pages/ApartmentsCreate.vue'
 import ApatmentEdit from './pages/ApatmentEdit.vue'
+import RegisterView from './pages/RegisterView.vue'
 
 export default createRouter({
   history: createWebHistory(),
   routes: [
     { path: '/', component: HomeView },
     { path: '/login', component: LoginView },
+    { path: '/register', component: RegisterView },
     { path: '/apartments', name:'apartments', component: ApartmentsView },
     { path: '/apartment/:slug', name:'apartment', component: ApartmentView, props:true },
     { path: '/apartments/create', name:'create', component: ApartmentsCreate },
