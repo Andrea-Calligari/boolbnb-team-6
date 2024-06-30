df8ef7d7962318a75df7c6ad0a2ed9e7be2385d8
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-06-30 20:25:14 +0200

    Add console log of messages of appartments

---
+++ front_office_vue/src/pages/ReceivedMessages.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+import axios from 'axios';
+import { store } from '../store';
+    export default {
+        data(){
+            return{
+                store,
+                messages:[]
+            }
+        },
+        methods:{
+            fetchMessage(){
+                axios.get('http://localhost:8000/api/messages').then((res)=>{
+                    for(let i = 0; i<res.data.results.messages.length; i++){
+
+                        this.messages.push( res.data.results.messages[i]) 
+                    }
+                    
+                    console.log(this.messages[0][0].id)
+                })
+            }
+            
+        },
+        mounted(){
+            this.fetchMessage()
+        }
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+
+</style>
