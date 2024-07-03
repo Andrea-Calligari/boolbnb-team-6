1a4334a491491e6db54435266e46f36233121cf6
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-07-03 10:53:52 +0200

    move address search to store and test in dashboard

--- front_office_vue/src/pages/dashboard/DashboardHome.vue
+++ front_office_vue/src/pages/dashboard/DashboardHome.vue
@@ -5,13 +5,13 @@
   <h1>dashboard</h1>
   <!-- <input @keyup="searchdd()" type="text" v-model="textt"> -->
 
-  <input @keyup="searchdd()" type="text" v-model="textt" list="position">
+  <input @keyup="store.address.searchAddresses(text)" type="text" v-model="text" list="position">
   <datalist id="position">
-    <option v-for="positio in position">{{ positio.address.freeformAddress }}</option>
+    <option v-for="position in store.address.listAddresses">{{ position.address.freeformAddress }}</option>
   </datalist>
 
-  <p v-for="positio in position">
-    {{ positio.address.freeformAddress }}
+  <p v-for="position in store.address.listAddresses">
+    {{ position.address.freeformAddress }}
   </p>
 
 
@@ -23,25 +23,12 @@ export default {
   data() {
     return {
       store,
-      textt: '',
-      position: null,
+      text: '',
+      
     }
   },
   methods: {
-    async searchdd() {
-      console.log(this.textt);
-      this.position = await fetch(`https://api.tomtom.com/search/2/search/${encodeURI(this.textt)}.json?key=orDHPznfE908Jeu45AKVaFSiSMAebYfQ`)
-        .then((response) => response.json())
-        .then((data) => {
-          console.log(data.results);
-
-          return data.results
-        })
-        .catch(function (error) {
-          reject(error);
-        });
-
-    }
+    
   },
   computed: {
   },
