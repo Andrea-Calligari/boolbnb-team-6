2be16210abee0d8311d178a22cbaeaff4733883d
AntoCic <anto.cic.127@gmail.com>
2024-07-03 20:44:46 +0200

    ADD 500 real apartment (seeder)

--- front_office_vue/src/pages/dashboard/DashboardHome.vue
+++ front_office_vue/src/pages/dashboard/DashboardHome.vue
@@ -3,17 +3,6 @@
 
 
   <h1>dashboard</h1>
-  <!-- <input @keyup="searchdd()" type="text" v-model="textt"> -->
-
-  <input @keyup="store.address.searchAddresses(text)" type="text" v-model="text" list="position">
-  <datalist id="position">
-    <option v-for="position in store.address.listAddresses">{{ position.address.freeformAddress }}</option>
-  </datalist>
-
-  <p v-for="position in store.address.listAddresses">
-    {{ position.address.freeformAddress }}
-  </p>
-
 
 </template>
 
@@ -23,12 +12,11 @@ export default {
   data() {
     return {
       store,
-      text: '',
-      
+
     }
   },
   methods: {
-    
+
   },
   computed: {
   },
