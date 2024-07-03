9c883aa29b44d4717da64b8d3d51bfed271fe8ec
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-07-03 16:45:32 +0200

    Fix the apartments image on show index e user apartments

--- front_office_vue/src/pages/apartment/ApartmentsIndex.vue
+++ front_office_vue/src/pages/apartment/ApartmentsIndex.vue
@@ -4,10 +4,12 @@
             <template v-if="apartments.length !== 0">
                 <div class="col" v-for="apartment in apartments" :key="apartment.id" :slug="apartment.slug">
                     <div class="card mb-3">
-                        <template v-for="(image, i) in apartment.image.split(',')" :key="'image'+ apartment.id">
-                            <img v-if="i === 0"
-                                :src="image ? 'http://localhost:8000/' + image : 'http://localhost:8000/uploads/apartment/img_default/null.png'"
-                                height="300" class="card-img-top" alt="...">
+                        <template v-if="apartment.image !== null">
+                            <div v-for="(image, i) in apartment.image.split(',')" :key="'image' + apartment.id">
+                                <img v-if="i === 0"
+                                    :src="image ? 'http://localhost:8000/' + image : 'http://localhost:8000/uploads/apartment/img_default/null.png'"
+                                    height="300" class="card-img-top" alt="...">
+                            </div>
                         </template>
                         <div class="card-body">
                             <h5 class="card-title">{{ apartment.title }}</h5>
--- front_office_vue/src/pages/dashboard/UserApartments.vue
+++ front_office_vue/src/pages/dashboard/UserApartments.vue
@@ -4,10 +4,12 @@
             <template v-if="store.user.apartments.length !== 0">
                 <div class="col" v-for="apartment in store.user.apartments" :key="apartment.id" :slug="apartment.slug">
                     <div class="card mb-3">
-                        <template v-for="(image, i) in apartment.image.split(',')" :key="'image'+ apartment.id">
-                            <img v-if="i === 0"
-                                :src="image ? 'http://localhost:8000/' + image : 'http://localhost:8000/uploads/apartment/img_default/null.png'"
-                                height="300" class="card-img-top" alt="...">
+                        <template v-if="apartment.image !== null">
+                            <template v-for="(image, i) in apartment.image.split(',')" :key="'image'+ apartment.id">
+                                <img v-if="i === 0"
+                                    :src="image ? 'http://localhost:8000/' + image : 'http://localhost:8000/uploads/apartment/img_default/null.png'"
+                                    height="300" class="card-img-top" alt="...">
+                            </template>
                         </template>
                         <div class="card-body">
                             <h5 class="card-title">{{ apartment.title }}</h5>
