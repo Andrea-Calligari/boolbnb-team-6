fed28185da0b65eca5a8b473ba1677c310609312
AntoCic <anto.cic.127@gmail.com>
2024-07-09 21:57:44 +0200

    ADD for statistic style

--- back_office_laravel/database/seeders/ViewSeeder.php
+++ back_office_laravel/database/seeders/ViewSeeder.php
@@ -17,7 +17,7 @@ class ViewSeeder extends Seeder
     {
         $apartments_ids = Apartment::pluck('id')->all();
 
-        for ($i = 0; $i < 20000; $i++) {
+        for ($i = 0; $i < 200000; $i++) {
             $view = new View();
             $view->apartment_id = $faker->randomElement($apartments_ids);
             $view->ip_address = $faker->ipv4();
--- front_office_vue/src/pages/dashboard/DashboardStatistic.vue
+++ front_office_vue/src/pages/dashboard/DashboardStatistic.vue
@@ -1,6 +1,28 @@
 <template>
-    <div>
-        <div style="width: 800px;"><canvas id="acquisitions"></canvas></div>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <div class="text-end">
+                    <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+                        <input type="radio" class="btn-check" name="radio-btn" id="giornaliero" autocomplete="off"
+                            checked>
+                        <label class="btn btn-outline-warning" @click="parseViews(10)" for="giornaliero">Giorni</label>
+
+                        <input type="radio" class="btn-check" name="radio-btn" id="mensile" autocomplete="off">
+                        <label class="btn btn-outline-warning" @click="parseViews(7)" for="mensile">Mesi</label>
+
+                        <input type="radio" class="btn-check" name="radio-btn" id="annuale" autocomplete="off">
+                        <label class="btn btn-outline-warning" @click="parseViews(4)" for="annuale">Anni</label>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+        <div class="row">
+            <div id="grafics">
+
+            </div>
+        </div>
     </div>
 </template>
 
@@ -24,41 +46,78 @@ export default {
     },
     //sommare tutte le visualizzazioni di una determinata data
     methods: {
-        parseViews() {
+        parseViews(sliceNumber = 10) {
+            this.data = [];
+            this.totalViews = [];
             for (let i = 0; i < this.views.length; i++) {
-                const day = this.views[i].viewed_at.slice(0, 10)
+                const day = this.views[i].viewed_at.slice(0, sliceNumber)
                 if (!this.data.includes(day)) {
                     this.data.push(day)
                 }
             }
 
             this.data.sort()
-            console.log(this.data)
 
             for (let i = 0; i < this.data.length; i++) {
                 let count = 0
                 for (let y = 0; y < this.views.length; y++) {
-                    if (this.data[i] === this.views[y].viewed_at.slice(0, 10)) {
+                    if (this.data[i] === this.views[y].viewed_at.slice(0, sliceNumber)) {
                         count++
                     }
                 }
                 this.totalViews.push(count);
             }
 
-            console.log(this.totalViews);
+            const grafics = document.getElementById('grafics');
 
+            grafics.innerHTML = '<canvas id="acquisitions" width="5000"></canvas>'
 
+            const typeStatics = {
+                '10': {
+                    type: 'line',
+                    label: 'Giorni',
+                },
+                '7': {
+                    type: 'polarArea',
+                    label: 'Mesi',
+                },
+                '4': {
+                    type: 'doughnut',
+                    label: 'Anni',
+                },
+            }
+            if (sliceNumber === 7) {
+                const months = {
+                    '01': 'gennaio',
+                    '02': 'febbraio',
+                    '03': 'marzo',
+                    '04': 'aprile',
+                    '05': 'maggio',
+                    '06': 'giugno',
+                    '07': 'luglio',
+                    '08': 'agosto',
+                    '09': 'settembre',
+                    '10': 'ottobre',
+                    '11': 'novembre',
+                    '12': 'dicembre'
+                }
+                let dataParsed = []
+                this.data.forEach(dataX => {
+                    dataParsed.push(months[dataX.slice(5)])
+                });
+                this.data = dataParsed;
 
+            }
 
             new Chart(
                 document.getElementById('acquisitions'),
                 {
-                    type: 'line',
+                    type: typeStatics[sliceNumber].type,
                     data: {
                         labels: this.data,
                         datasets: [
                             {
-                                label: 'Acquisitions by year',
+                                label: typeStatics[sliceNumber].label,
                                 data: this.totalViews
                             }
                         ]
@@ -75,11 +134,7 @@ export default {
 
 
         axios.get(`http://localhost:8000/api/apartments/${this.slug}/data`).then((res) => {
-
-            // console.log(res.data.results[0]);
-            //this.apartment = res.data.apartment;
             this.views = res.data.apartment.views
-            //console.log(this.views)
 
             this.parseViews()
 
