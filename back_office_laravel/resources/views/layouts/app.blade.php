d776a7e7513f5867bc5835406db266e3efc50ff0
AntoCic <anto.cic.127@gmail.com>
2024-07-09 22:14:14 +0200

    REMOVED payment fro laravel blade

--- back_office_laravel/resources/views/layouts/app.blade.php
+++ back_office_laravel/resources/views/layouts/app.blade.php
@@ -11,12 +11,6 @@
 
     <title>{{ config('app.name', 'Laravel') }}</title>
 
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
-    <script src="https://js.braintreegateway.com/web/dropin/1.8.1/js/dropin.min.js"></script>
-
-    
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
@@ -105,36 +99,6 @@
             @yield('content')
         </main>
     </div>
-    <div class="container">
-        <div class="row">
-            <div class="col-md-8 col-md-offset-2">
-                <div id="dropin-container"></div>
-                <button id="submit-button">Request payment method</button>
-            </div>
-        </div>
-    </div>
-    <script>
-        var button = document.querySelector('#submit-button');
-
-        braintree.dropin.create({
-            authorization: "{{ Braintree\ClientToken::generate() }}",
-            container: '#dropin-container'
-        }, function(createErr, instance) {
-            button.addEventListener('click', function() {
-                instance.requestPaymentMethod(function(err, payload) {
-                    $.get('{{ route('payment.process') }}', {
-                        payload
-                    }, function(response) {
-                        if (response.success) {
-                            alert('Payment successfull!');
-                        } else {
-                            alert('Payment failed');
-                        }
-                    }, 'json');
-                });
-            });
-        });
-    </script>
 </body>
 
 </html>
