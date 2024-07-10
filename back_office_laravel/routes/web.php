48a08f92c3d1083a4a2b911bbdc4092c0e8c0e11
Carmine-Doronzo <carmine.doronzo92@gmail.com>
2024-07-10 14:42:20 +0200

    refactoring back-office

--- back_office_laravel/routes/web.php
+++ back_office_laravel/routes/web.php
@@ -17,24 +17,8 @@ use Illuminate\Support\Facades\Route;
 */
 // Route::get('/payment/process',[PaymentsController::class, 'process'])->name('payment.process');
 Route::get('/', function () {
-    return view('welcome');
+    return redirect('http://localhost:5173');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware(['auth','verified'])
-->name('admin.')
-->prefix('admin')
-->group(function (){
-    Route::resource('apartments', ApartmentController::class);
-});
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
 
 require __DIR__.'/auth.php';
